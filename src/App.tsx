c1978bca000478c8109e48bf328645c49c188231
george-steven0 <george.steven10020@gmail.com>
2025-03-07 14:02:49 +0200

    create env file locale

--- src/App.tsx
+++ src/App.tsx
@@ -2,7 +2,10 @@ import axios from "axios";
 import "./App.css";
 
 function App() {
-  axios.defaults.baseURL = "http://localhost:8081";
+  // axios.defaults.baseURL = "http://localhost:8081";
+  axios.defaults.baseURL = import.meta.env.VITE_API_URL;
+  console.log(import.meta?.env.VITE_API_URL);
+
   axios.defaults.headers.post["Content-Type"] = "application/json";
   axios.defaults.headers.post["Access-Control-Allow-Origin"] = "*";
   return <></>;
